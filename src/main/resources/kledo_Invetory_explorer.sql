df148855a31cbebcf413310bb07a811446a32eb9
Eric_Raw <112918665+EricRaw512@users.noreply.github.com>
2024-04-17 14:16:52 +0700

    Add files via upload
    Add sql to resource

---
+++ src/main/resources/kledo_Invetory_explorer.sql
@@ -0,0 +1,3 @@
+-- Insert Admin into the database with username admin and password qwerty123 --
+INSERT INTO kledo_inventory_explorer.users (id, username, password, role) 
+VALUES (1, 'admin', '$2a$10$4tj6.DNLE5YKOrYhyVlzHeZoOzNwSPsYNvcQPcC9mBEGAF2YMX2G.', 'ADMIN');
